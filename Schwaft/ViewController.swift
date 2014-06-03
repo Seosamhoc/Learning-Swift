843fab5819b61676e543d113b555379b12687a25
seosamhoc <seosamh.ocinneide@gmail.com>
2014-06-03 19:50:19 +0100

    Initial Commit

---
+++ Schwaft/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schwaft
+//
+//  Created by Seosamh Ó Cinnéide on 03/06/2014.
+//  Copyright (c) 2014 Seosamh Ó Cinnéide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
